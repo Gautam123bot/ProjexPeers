e18823f7c57d8a0841d4d39274c32612a6955345
Gautam <0101gautakumar@gmail.com>
2024-12-29 15:47:23 +0530

    corrected bcrypt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
 import User from "../models/user.js"; 
 import sgMail from "@sendgrid/mail";
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose the server to external hosts
+    port: 5173,      // Ensure this matches the port used in Docker
+  },
 })
